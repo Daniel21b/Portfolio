fdf6ca341ef9b21f732531a70274f752d8776fce
Daniel Berhane <d.asfaw10@gmail.com>
2026-01-16 14:53:42 -0500

    fixed build blog

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { routing } from '@/i18n/routing';
-import { count, sum } from 'drizzle-orm';
+import { count } from 'drizzle-orm';
 import { getTranslations } from 'next-intl/server';
 import { redirect } from 'next/navigation';
 
@@ -8,9 +8,8 @@ import Metric from '@/components/metric';
 import { SignOutButton } from '@/components/sign-out';
 import Track from '@/components/track';
 import { auth } from '@/lib/auth';
-import { getBlogPosts } from '@/lib/blog';
 import { db } from '@/lib/db';
-import { guestbook, views } from '@/lib/db/schema';
+import { guestbook } from '@/lib/db/schema';
 import { getTopTracks } from '@/lib/spotify';
 
 import type { Metadata } from 'next/types';
@@ -31,17 +30,6 @@ export function generateMetadata(): Metadata {
   };
 }
 
-const getViewsCount = async () => {
-  const data = await db
-    .select({
-      total: sum(views.count),
-    })
-    .from(views)
-    .execute();
-
-  return data[0]?.total ?? 0;
-};
-
 const getGuestbookEntriesCount = async () => {
   const data = await db
     .select({
@@ -55,11 +43,10 @@ const getGuestbookEntriesCount = async () => {
 
 const DashboardPage = async ({ params }: PageProps<'/[locale]/dashboard'>) => {
   const locale = (await params).locale as (typeof routing.locales)[number];
-  const [session, t, viewsCount, guesbookEntriesCount, topTracks] =
+  const [session, t, guestbookEntriesCount, topTracks] =
     await Promise.all([
       auth(),
       getTranslations({ locale, namespace: 'dashboard' }),
-      getViewsCount(),
       getGuestbookEntriesCount(),
       getTopTracks(),
     ]);
@@ -83,12 +70,8 @@ const DashboardPage = async ({ params }: PageProps<'/[locale]/dashboard'>) => {
       </p>
       <div className="my-2 grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
         <h2 className="mt-3 text-xl font-bold sm:col-span-2">{t('stats')}</h2>
-        <Metric title={t('metrics.blog-views')}>{viewsCount}</Metric>
-        <Metric title={t('metrics.posts-count')}>
-          {getBlogPosts().filter((x) => x.language === locale).length}
-        </Metric>
         <Metric title={t('metrics.guestbook-entries')}>
-          {guesbookEntriesCount}
+          {guestbookEntriesCount}
         </Metric>
       </div>
       <h2 className="mb-4 mt-16 text-3xl font-bold tracking-tight text-black dark:text-white">
